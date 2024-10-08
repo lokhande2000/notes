b10a676a45c9c021ff13af12f1237c992877cd90
suraj <surajlokhande7744@gmail.com>
2024-10-08 07:35:55 +0530

    note app

---
+++ notes-app/src/App.jsx
@@ -0,0 +1,14 @@
+import { Box, Heading } from '@chakra-ui/react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <Box minH='100vh' bg='blackAlpha.500' textAlign='center'>
+      <Heading as='h1' fontSize="7xl">notes app</Heading>
+    </Box>
+  )
+}
+
+export default App
